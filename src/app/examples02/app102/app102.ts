2cc4ca94dc35af7c436c81cb710574440293476a
Paolo <paolo@example.com>
2026-06-23 15:20:13 +0200

    esempio 2 con Model Input

---
+++ src/app/examples02/app102/app102.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app102',
+  imports: [],
+  templateUrl: './app102.html',
+  styleUrl: './app102.css',
+})
+export class App102 {}
---
+++ src/app/examples02/quantity-selector/quantity-selector.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quantity-selector',
+  imports: [],
+  templateUrl: './quantity-selector.html',
+  styleUrl: './quantity-selector.css',
+})
+export class QuantitySelector {}
